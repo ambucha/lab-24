7c940114e521e47dee35a683ca381271ad2bec82
ambucha <ambuchatskiy@gmail.com>
2025-10-26 13:28:44 -0700

    third commit

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
-int select_goat(list<Goat> trip);
-void delete_goat(list<Goat> &trip);
-void add_goat(list<Goat> &trip, string [], string []);
-void display_trip(list<Goat> trip);
+int select_goat(set<Goat> trip);
+void delete_goat(set<Goat> &trip);
+void add_goat(set<Goat> &trip, string [], string []);
+void display_trip(const set<Goat>& trip);
 int main_menu();
 
 int main() {
@@ -51,6 +51,7 @@ int main() {
     }
     */
 
+    add_goat(trip, names, colors);
     display_trip(trip);
 
     return 0;
@@ -70,52 +71,21 @@ int main_menu(){
 }
 
 void add_goat(set<Goat> &trip, string names[], string colors[]){
-    // create a temportary Goat variable with random name, age, and color
     Goat temp(names[rand() % SZ_NAMES], rand() % MAX_AGE,colors[rand() % SZ_COLORS]);
-
-    // add this temporary varaible to the list
     trip.insert(temp);
 }
 
 void delete_goat(set<Goat> &trip){
-    // start my displaying the goats in the trip atm
-    // wait i lowk need to check if the list is mepty frist befiore deleting anything
-    if(trip.empty()) {
-        cout << "No goats on the trip right now" << endl;
-        return;
-    }
-
-    display_trip(trip);
-    int i;
-
-    cout << "What goat would you like to eliminate: " << endl;
-    cin >> i;
-    cout << endl;
-
-    // also need to check if it can even be taken out (if it is withijn the list bounds)
-    while(i < 1 || i > trip.size()){
-        cout << "This goat does not exist, try giving me a valid goat number: ";
-        cin >> i;
-        cout << endl;
-    }
-
-    // use .erase() with .begin() 
-    auto it = trip.begin();
-    for(int k = 0; k < i - 1; k++){
-        it++;
-    }
-
-    // erase the goat at given choice
-    trip.erase(it);
+    
 }
 
-void display_trip(set<Goat> trip){
+void display_trip(const set<Goat>& trip){
     // counter to display which goat you are on
     int count = 1;
 
     // for loop to go through each goat in the list and display its stats
     for(auto it = trip.begin(); it != trip.end(); it++){
-        cout << "[" << count << "] " << (*it).get_name() << " (" << (*it).get_age() << ", " << (*it).get_color() << ")" << endl;
+        cout << "[" << count << "] " << it->get_name() << " (" << it->get_age() << ", " << it->get_color() << ")" << endl;
         count++;
     }
 
